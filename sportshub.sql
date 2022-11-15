ffd7bf9cb57de9122e2897324769fdb1a5ecb89d
sarkermajid <majidursarkar333@gmail.com>
2022-11-15 15:05:47 +0600

    'sportshub'

--- sportshub.sql
+++ sportshub.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 12, 2022 at 03:31 AM
+-- Generation Time: Nov 15, 2022 at 10:05 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.0.19
 
@@ -39,7 +39,8 @@ CREATE TABLE `admins` (
 --
 
 INSERT INTO `admins` (`id`, `name`, `email`, `password`) VALUES
-(1, 'admin', 'adminmain@gmail.com', 'admin123');
+(1, 'admin', 'admin@gmail.com', 'admin123'),
+(2, 'sarkermajid', 'sarkermajid@gmail.com', 'f18e1f80099800fb4d122199a569df66');
 
 -- --------------------------------------------------------
 
@@ -57,11 +58,8 @@ CREATE TABLE `categories` (
 --
 
 INSERT INTO `categories` (`id`, `category_name`) VALUES
-(1, 'Hocky'),
-(3, 'Cricket'),
-(4, 'Football'),
-(5, 'DABA'),
-(6, 'T20');
+(11, 'football'),
+(12, 'cricket');
 
 -- --------------------------------------------------------
 
@@ -73,8 +71,8 @@ CREATE TABLE `newses` (
   `id` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
   `author` varchar(255) NOT NULL,
-  `category_name` varchar(255) NOT NULL,
-  `content` text NOT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `content` longtext NOT NULL,
   `image` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -82,9 +80,11 @@ CREATE TABLE `newses` (
 -- Dumping data for table `newses`
 --
 
-INSERT INTO `newses` (`id`, `title`, `author`, `category_name`, `content`, `image`) VALUES
-(4, 'fdvb hdfhgdhg', 'v xdfvbfdvbfdbgv', 'football', 'fdvb hdfhgdhg', 'assets/img/86616223_1304381239746537_1914458371994419200_n.jpg'),
-(5, 'fdvb hdfhgdhg', 'v xdfvbfdvbfdbgv', 'cricket', 'fdvb hdfhgdhg', 'assets/img/118772524_1468180213366638_8787847572089559951_n.jpg');
+INSERT INTO `newses` (`id`, `title`, `author`, `category_id`, `content`, `image`) VALUES
+(14, 'From upsets to England joy, a T20 WC to remember', 'sarker', 12, 'The curtain on the Twenty20 World Cup 2022 dropped last Sunday with England finishing as the latest world champions, defeating Pakistan in dominant fashion by five wickets at the historic Melbourne Cricket Ground.', 'assets/img/32nf3xz-highres.jpg'),
+(15, 'Buttler: We deserved to be world champions', 'sarker', 12, 'Twenty20 World Cup winning skipper Jos Buttler firmly believes England have won the trophy as the deserved winners as they played their best cricket throughout the tournament with both bat and ball, and outplayed their opponents. ', 'assets/img/000-32nf46a.jpeg'),
+(16, 'Mbappe on target for five-star PSG', 'sarkermajid', 11, 'Kylian Mbappe scored and came through unscathed along with fellow Paris Saint-Germain stars Lionel Messi and Neymar in their final match before the World Cup, a 5-0 Ligue 1 rout of Auxerre Sunday', 'assets/img/up-56.jpeg'),
+(17, 'Two-time champions Argentina begin their World Cup', 'sarkermajid', 11, 'Captain Lionel Messi will lead the squad in Qatar at his fifth and probably last World Cup.', 'assets/img/000-32nc4xg.jpeg');
 
 --
 -- Indexes for dumped tables
@@ -106,7 +106,8 @@ ALTER TABLE `categories`
 -- Indexes for table `newses`
 --
 ALTER TABLE `newses`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `category_id` (`category_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -116,19 +117,29 @@ ALTER TABLE `newses`
 -- AUTO_INCREMENT for table `admins`
 --
 ALTER TABLE `admins`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `newses`
 --
 ALTER TABLE `newses`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `newses`
+--
+ALTER TABLE `newses`
+  ADD CONSTRAINT `category_id` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
